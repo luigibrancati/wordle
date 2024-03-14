e25a90015025196f6cbccdc556cdb3be4061fbd6
luigibrancati <luigi.brancati@hype.it>
2024-03-14 20:16:09 +0100

    Added create user endpoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
@@ -7,12 +7,25 @@ from fastapi.responses import RedirectResponse
 from wordle_status import WordleStatus
 from fastapi import status
 import os
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 templates = Jinja2Templates(directory=os.path.dirname(os.path.abspath(__file__)) + "/templates")
 static_path = os.path.dirname(os.path.abspath(__file__)) + "/static"
 app.mount(static_path, StaticFiles(directory=static_path), name="static")
 wordle_status = WordleStatus()
+
+models.Base.metadata.create_all(bind=engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -45,3 +58,11 @@ async def remove_letter():
 async def reset():
     wordle_status.reset()
     return RedirectResponse("/", status_code=status.HTTP_302_FOUND)
+
+
+@app.post("/create_user", response_model=schemas.User)
+async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_name(db, name=user.name)
+    if db_user:
+        raise HTTPException(status_code=400, detail="User already registered")
+    return crud.create_user(db=db, user=user)
