bde3549a932253fa75fc566242f975771e6af974
luigi.brancati <luigi.brancati93@gmail.com>
2024-03-21 03:59:30 +0100

    Added perc

--- wordle/routers/dashboard.py
+++ wordle/routers/dashboard.py
@@ -14,8 +14,9 @@ import pandas as pd
 router = APIRouter(prefix='/dashboard')
 
 
-def success_pie_chart(user: Annotated[schemas.User, Depends(auth_utils.manager)], db: Annotated[Session, Depends(get_db)]):
+def game_stats(user: Annotated[schemas.User, Depends(auth_utils.manager)], db: Annotated[Session, Depends(get_db)]):
     df = pd.DataFrame({"id": [g.id for g in user.games], "points": [g.points for g in user.games], "result": [g.won for g in user.games]})
+    winning_perc = df[df['result']].size / df.size
     df['result'] = df['result'].replace(True, 'won').replace(False, 'lost')
     chart = px.pie(
         data_frame=df,
@@ -23,17 +24,17 @@ def success_pie_chart(user: Annotated[schemas.User, Depends(auth_utils.manager)]
         height=300,
         width=400
     )
-    chart.update_traces(textposition='inside', textinfo='percent+label')
+    chart.update_traces(textposition='inside', textinfo='value+label')
     chart.update_layout(
         margin=dict(l=0,r=0,t=0,b=0),
         showlegend=False
     )
-    return chart
+    return chart, winning_perc
 
 
 @router.get("/", response_class=HTMLResponse)
 async def show_dashboard(request: Request, user: Annotated[schemas.User, Depends(auth_utils.manager)], db: Annotated[Session, Depends(get_db)]):
-    chart = success_pie_chart(db=db, user=user)
+    chart, winning_perc = game_stats(db=db, user=user)
     return templates.TemplateResponse(
-        request=request, name="dashboard/dashboard.html", context={"user": user, "success_pie_chart": chart.to_html(full_html=False)}, status_code=200
+        request=request, name="dashboard/dashboard.html", context={"user": user, "winning_pie_chart": chart.to_html(full_html=False), "winning_perc": winning_perc}, status_code=200
     )
